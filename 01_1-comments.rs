4812231d976954b019ffaa82d54da3d06bb64c0f
Lorenz29 <lorenzohardoy@hotmail.com>
2024-05-24 19:47:10 -0300

    Activity 1.1

---
+++ 01_1-comments.rs
@@ -0,0 +1,10 @@
+fn main(){
+    // This is a line comment
+    /*  This is a block comment
+        new line
+        new line
+        close comment
+    */
+    let x = 5 + 90 + 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
