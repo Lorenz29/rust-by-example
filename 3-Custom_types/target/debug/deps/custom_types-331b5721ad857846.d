ea1c939d3012692f4b05bd444da7c1803fb06e7e
Lorenz29 <lorenzohardoy@hotmail.com>
2024-05-30 16:32:55 -0300

    Cargo test

---
+++ 3-Custom_types/target/debug/deps/custom_types-331b5721ad857846.d
@@ -0,0 +1,5 @@
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/custom_types-331b5721ad857846: src/main.rs
+
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/custom_types-331b5721ad857846.d: src/main.rs
+
+src/main.rs:
---
+++ 3-Custom_types/target/debug/deps/custom_types-5356a2ad8d70313a.d
@@ -0,0 +1,5 @@
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/libcustom_types-5356a2ad8d70313a.rmeta: src/main.rs
+
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/custom_types-5356a2ad8d70313a.d: src/main.rs
+
+src/main.rs:
