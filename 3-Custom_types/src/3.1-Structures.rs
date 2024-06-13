f45f88af52e7e569c01fda86d5a8df6f987991c2
Lorenz29 <lorenzohardoy@hotmail.com>
2024-06-13 17:50:21 -0300

    Activity 3.1

--- 3-Custom_types/src/3.1-Structures.rs
+++ 3-Custom_types/src/3.1-Structures.rs
@@ -34,7 +34,7 @@ fn main() {
     let peter = Person { name, age };
 
     // Print debug struct
-    println!("{:?}", peter);
+    println!("{}", peter);
 
     // Instantiate a `Point`
     let point: Point = Point { x: 10.3, y: 0.4 };
