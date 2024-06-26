e2309e267f23bec6ed5f8de80e888144d2154b7c
Lorenz29 <lorenzohardoy@hotmail.com>
2024-06-26 16:44:18 -0300

    Activity 3.2

---
+++ 3-Custom_types/target/debug/Enums.d
@@ -0,0 +1,1 @@
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/Enums: /home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/src/3.2-Enums.rs
---
+++ 3-Custom_types/target/debug/deps/Enums-a96f8203769167f7.d
@@ -0,0 +1,5 @@
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/Enums-a96f8203769167f7: src/3.2-Enums.rs
+
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/Enums-a96f8203769167f7.d: src/3.2-Enums.rs
+
+src/3.2-Enums.rs:
