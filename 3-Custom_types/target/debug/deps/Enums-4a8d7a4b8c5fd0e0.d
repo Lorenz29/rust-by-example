e4c2de0065137297d47165fd62292339d68ef94b
Lorenz29 <lorenzohardoy@hotmail.com>
2024-08-09 15:03:44 -0300

    Custom types

---
+++ 3-Custom_types/target/debug/deps/Enums-4a8d7a4b8c5fd0e0.d
@@ -0,0 +1,5 @@
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/libEnums-4a8d7a4b8c5fd0e0.rmeta: src/3.2-Enums.rs
+
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/Enums-4a8d7a4b8c5fd0e0.d: src/3.2-Enums.rs
+
+src/3.2-Enums.rs:
---
+++ 3-Custom_types/target/debug/deps/Enums-91daa566701f498f.d
@@ -0,0 +1,5 @@
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/libEnums-91daa566701f498f.rmeta: src/3.2-Enums.rs
+
+/home/lorenz29/Trabajo/Development/Rust/rust-by-example/rust-by-example/3-Custom_types/target/debug/deps/Enums-91daa566701f498f.d: src/3.2-Enums.rs
+
+src/3.2-Enums.rs:
